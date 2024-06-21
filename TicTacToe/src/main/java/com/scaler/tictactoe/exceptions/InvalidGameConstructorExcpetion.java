06659ff71d9bdcd333bf6615951820233605a084
LENOVO <kuruvaveeresh7833@gmail.com>
2024-06-21 16:42:01 +0530

    my first change

--- TicTacToe/src/main/java/com/scaler/tictactoe/exceptions/InvalidGameConstructorExcpetion.java
+++ TicTacToe/src/main/java/com/scaler/tictactoe/exceptions/InvalidGameConstructorExcpetion.java
@@ -1,7 +1,7 @@
 package com.scaler.tictactoe.exceptions;
 
 public class InvalidGameConstructorExcpetion extends Exception {
-
+//
     public InvalidGameConstructorExcpetion(String message) {
         super(message);
     }
